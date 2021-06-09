cbb974acf83060347787f37db27708ab25d98f0e
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-09 12:17:34 +0530

    onClick handler to change language

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    }
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i onClick={() => this.onLanguageChange('english')} className="flag us" />
+                    <i onClick={() => this.onLanguageChange('dutch')} className="flag nl" />
+                </div>
+                {this.state.language}
+            </div>
         )
     }
 }
